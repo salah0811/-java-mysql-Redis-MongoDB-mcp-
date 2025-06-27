1b3ae1eb73d9b6199625c0f3fbb7da19a4155e84
wgy <860648654@qq.com>
2025-06-27 11:10:07 +0800

    修改依赖版本不符问题

--- src/main/java/com/siddhu/SpringBootAiMongoMcpServerApplication.java
+++ src/main/java/com/siddhu/SpringBootAiMongoMcpServerApplication.java
@@ -8,6 +8,9 @@ public class SpringBootAiMongoMcpServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAiMongoMcpServerApplication.class, args);
+		System.out.println("~~~~~~~~~~~~~~~~MCP服务启动成功~~~~~~~~~~~~~~~~~");
+		System.out.println("~~~~~~~~~~~~~~~~MCP服务启动成功~~~~~~~~~~~~~~~~~");
+		System.out.println("~~~~~~~~~~~~~~~~MCP服务启动成功~~~~~~~~~~~~~~~~~");
 	}
 
 }
